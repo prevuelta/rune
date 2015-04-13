91ec9bb63f162a1d322c62c26e78719d92c18026
Pablo <suspekt007@gmail.com>
2015-04-13 22:46:22 +1200

    Getting head round tangenets

--- src/scripts/controllers/app.js
+++ src/scripts/controllers/app.js
@@ -1,9 +1,9 @@
 var RuneGrid = {
 	
-	xUnits: 4,
-	yUnits: 4,
-	xRes: 50,
-	yRes: 50,
+	xUnits: 6,
+	yUnits: 6,
+	xRes: 40,
+	yRes: 40,
 	silverRatio: Math.sqrt(2),
 	goldenRatio: (1 + Math.sqrt(5)) / 2,
 
@@ -18,24 +18,24 @@ var RuneGrid = {
 		var that = this;
 
 		var letter = {
-			"gridPoints" : [12, 0, 1, 10, 2, 3, 15, 14, 5, 13],
-			"distortions" : [
+			"gridPoints" : [31, 30, 0, 1, 28, 4, 5, 35, 34, 7],
+			"distortions" : 
 				{
 					"type" : "tri",
-					"index" : 8,
-					"dir" : "down",
-					"p1" : 2,
-					"p2" : 3
-				},
-				{
-					"type" : "tri",
-					"index" : 3,
-					"dir" : "up",
-					"p1" : 7,
-					"p2" : 8
+					"points" : [1, 28, 34, 7],
 				}
-			],
+				// {
+				// 	"type" : "tri",
+				// 	"index" : 3,
+				// 	"dir" : "up",
+				// 	"p1" : 7,
+				// 	"p2" : 8
+				// }
+			,
 			"rendered" : [],
+			init: function() {
+
+			},
 			render : function(grid) {
 				console.log(this);
 				var that = this;
@@ -46,47 +46,115 @@ var RuneGrid = {
 					renderTemp.push(grid[point]);
 				});
 
-				$.each(this.distortions, function(idx, dist) {
-					that.distort(dist.dir, renderTemp[dist.index], renderTemp[dist.p1], renderTemp[dist.p2]);
+				console.log(this.distortions.points);
+
+				var indices = this.getIndices(this.distortions.points, this.gridPoints);
+
+				var punits = indices.map(function(idx) {
+					return renderTemp[idx];
 				});
+
+				that.correct(punits);
 
 				that.rendered = renderTemp;
 			},
-			distort: function(dir, point, p1, p2) {
+			getIndices: function(points, gridPoints) {
+				console.log(points);
+				return points.map(function(point) {
+					return gridPoints.indexOf(point);
+				});
+			},
+			correct: function(points) {
 
-				var angle = getAngle(p1, p2);
+				// var angle = getAngle(points[0], points[2]);
 
-				console.log("angle: " + angle);
+				// console.log("angle: " + angle);
+
+				var testPath = new paper.Path();
+
+				testPath.strokeColor = 'black';
+
+				testPath.moveTo(points[0]);
+				testPath.lineTo(points[2]);
+
+				var x = ((points[2].x - points[0].x) / 2) + points[0].x;
+				var y = ((points[2].y - points[0].y) / 2) + points[0].y;
+
+				var midPoint = new paper.Point(x, y);
+
+				var circle = new paper.Path.Circle(midPoint, that.xRes / 2);
+				circle.strokeColor = 'black'
+
+				var hyp = points[0].getDistance(midPoint);
+
+				var line = new paper.Point(points[2]);
+
+				console.log(line);
+
+				var adj = that.xRes / 2;
+
+				var side = Math.sqrt(hyp*hyp - adj*adj);
+
+				line.angle = (Math.acos( adj / hyp) * (180/Math.PI));
+
+				var nLine = line.normalize();
+
+				console.log(side);
+
+				nLine.length = nLine.length * side;
+
+				console.log(nLine);
+
+				var newPoint = points[2].subtract(nLine);
+
+
+				console.log(newPoint);
+
+				testPath.lineTo(newPoint);
 
 				function getAngle(p1, p2) {
-					var adj = that.xRes;
+					// var adj = that.xRes;
+
+					var adj = p1.getDistance(new paper.Point(p2.x, p1.y));
 					var hyp = p1.getDistance(p2);
 
-					return Math.acos( adj / hyp );
+					// cos() = a / h;
+
+					return (Math.PI / 2) - Math.acos( adj / hyp );
+
 				}
 
-				var adj = that.xRes;
+				// SOH				
 
-				var hyp = adj / Math.cos(angle);
+				// o / sin() = h;
 
-				console.log("Hyp" + hyp);
+				// var opp = that.xRes;
 
-				switch(dir) {
-					case "down" :
-						point.y += hyp - point.y;
-					break;
+				// var hyp =  opp / Math.sin(angle);
 
-					case "up" :
-						point.y -= hyp - point.y;
-					break;
-				}
+				// console.log("Hyp: " + hyp);
+
+
+				// switch(dir) {
+				// 	case "down" :
+				// points[3].y = hyp;
+
+				// var dist = points[1].getDistance(points[2]);
+
+				// points[1].y -= hyp - dist;
+				// 	break;
+
+				// 	case "up" :
+				// 		point.y -= hyp - point.y;
+				// 	break;
+				// }
 
 				
 
 			}
 		};
 
-		console.log(letter);
+		// console.log(letter);
 
 		this.canvas = document.getElementById('rune-grid');
 		// Create an empty project and a view for the canvas:
@@ -101,15 +169,15 @@ var RuneGrid = {
 			new paper.Point(0, 0)
 		);
 
-		this.gridLayers[1] =  this.setupGrid(
-			paper, 
-			this.totalUnits(), 
-			this.yUnits,
-			this.xRes,
-			this.yRes,
-			// new paper.Point((20 * Math.sqrt(2)) - 20, 0)
-			new paper.Point(0, (this.yRes * this.silverRatio) - this.yRes)
-		);
+		// this.gridLayers[1] =  this.setupGrid(
+		// 	paper, 
+		// 	this.totalUnits(), 
+		// 	this.yUnits,
+		// 	this.xRes,
+		// 	this.yRes,
+		// 	// new paper.Point((20 * Math.sqrt(2)) - 20, 0)
+		// 	new paper.Point(0, (this.yRes * this.silverRatio) - this.yRes)
+		// );
 
 		var grid = this.gridLayers[0];
 
@@ -119,7 +187,7 @@ var RuneGrid = {
 		var path = new paper.Path();
 		// Give the stroke a color
 		path.strokeColor = 'black';
-		path.fillColor = '#e9e9ff';
+		path.fillColor = new paper.Color(255, 0, 0, 0.2);
 
 		var start = new paper.Point(0, 0);
 		// Move to start and draw a line from there
@@ -131,17 +199,17 @@ var RuneGrid = {
 			switch(idx) {
 				case 1 :
 					circles = false;
-					color = 'red';
+					color = new paper.Color(255, 0, 0, 0.2)
 				break;
 			}
-			that.drawPoints(grid, color, paper, circles);
+			// that.drawPoints(grid, color, paper, circles);
 		});
 		
 		var letterLayer = new paper.Layer();
 
 		var secondPath = new paper.Path();
 
-		path.strokeColor = 'red';
+		path.strokeColor = new paper.Color(255, 255, 0, 0.2);
 
 		$.each(letter.rendered, function(idx, point) {
 			if(!idx) {
@@ -175,7 +243,7 @@ var RuneGrid = {
 		var gridColor = new paper.Color(255, 0, 0, 0.2);
 
 		$.each(locations, function(idx, point) {
-			console.log(point);
+			// console.log(point);
 			if(circles) {
 				var circle = new paper.Path.Circle(point, RuneGrid.xRes * Math.sqrt(2));
 				circle.strokeColor = gridColor;
@@ -183,13 +251,13 @@ var RuneGrid = {
 
 			var recPath = new paper.Path.Rectangle(point, RuneGrid.xRes);
 			recPath.strokeColor = gridColor;
-			
-			var pointText = new paper.PointText({point: point, content: idx});
+
+			var pointText = new paper.PointText({point: new paper.Point(point.x, point.y+10), content: idx});
 			pointText.fillColor = color;
 
-			pointText.onMouseDown = function(e) {
-				console.log(e.target._content);
-			}
+			// pointText.onMouseDown = function(e) {
+				// console.log(e.target._content);
+			// }
 			// recPath.fillColor = color;
 			// var path = paper.Rectangle(rec);
 		});
