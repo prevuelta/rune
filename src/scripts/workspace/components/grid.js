827414102c000827007cd5cd9ffeb4beefc0af00
Pablo <hello@pablorevuelta.com>
2017-05-22 16:45:22 +1200

    Member ship

--- src/scripts/workspace/components/grid.js
+++ src/scripts/workspace/components/grid.js
@@ -6,12 +6,6 @@ import Group from './group';
 //import Styles = from '../../util/styles';
 import { COLORS } from '../../util/constants';
 
-export default function GridNode (props) {
-    let {location, clickHandler, size} = props;
-    return (
-        <rect x={location[0]} y={location[1]} width={size} height={size} fill={"red"} onClick={clickHandler} fillOpacity={0.4} />
-    );
-}
 
 export function GridLines (props) {
     let { data } = props;
@@ -24,9 +18,9 @@ export function GridLines (props) {
     for (let i = 0; i <= Math.max(data.x, data.y); i++) {
         for (let j = 0; j < 5; j++){
             if (i < data.x)
-                lines.push(<Vline x={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={2000} />);
+                lines.push(<Vline x={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={width} />);
             if (i < data.y)
-                lines.push(<Hline y={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={2000} />);
+                lines.push(<Hline y={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={height} />);
         };
         if (i <= data.y)
             lines.push(<Hline y={i*data.gridUnit} color={COLORS.BLUE} length={2000} />);
@@ -50,11 +44,18 @@ export function GridNodes (props) {
     for (let x = 0; x <= data.x; x++) for (let y = 0; y <= data.y;y++) {
         let point = {x:x*data.gridUnit, y:y*data.gridUnit};
         let k = x*(data.y+1)+y;
-        nodes.push(<GridNode grid={[x,y]} key={k} clickHandler={clickHandler.bind(null, point)} location={[data.gridUnit * x, data.gridUnit * y]}  size={data.gridUnit/2} />);
+        nodes.push(<GridNode grid={[x,y]} key={k} clickHandler={clickHandler.bind(null, point)} location={[data.gridUnit * x, data.gridUnit * y]} size={data.gridUnit/2} />);
     };
     return (
         <Group>
             { nodes }
         </Group>
+    );
+}
+
+function GridNode (props) {
+    let {location, clickHandler, size} = props;
+    return (
+        <rect className="grid-node" x={location[0]} y={location[1]} width={size} height={size} fill={"red"} onClick={clickHandler} fillOpacity={0.4} />
     );
 }
