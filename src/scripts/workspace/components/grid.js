eb8c3e9cd69b3f1e6ae890cb0dd60981f9ecf715
Pablo <hello@pablorevuelta.com>
2017-05-22 17:30:30 +1200

    Member ship

--- src/scripts/workspace/components/grid.js
+++ src/scripts/workspace/components/grid.js
@@ -18,18 +18,18 @@ export function GridLines (props) {
     for (let i = 0; i <= Math.max(data.x, data.y); i++) {
         for (let j = 0; j < 5; j++){
             if (i < data.x)
-                lines.push(<Vline x={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={width} />);
+                lines.push(<Vline x={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={height} />);
             if (i < data.y)
-                lines.push(<Hline y={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={height} />);
+                lines.push(<Hline y={i *data.gridUnit+(j*data.gridUnit/5)} opacity={0.2} color={COLORS.BLUE} length={width} />);
         };
         if (i <= data.y)
-            lines.push(<Hline y={i*data.gridUnit} color={COLORS.BLUE} length={2000} />);
+            lines.push(<Hline y={i*data.gridUnit} color={COLORS.BLUE} length={width} />);
         if (i <= data.x)
-            lines.push(<Vline x={i*data.gridUnit} color={COLORS.BLUE} length={2000} />);
+            lines.push(<Vline x={i*data.gridUnit} color={COLORS.BLUE} length={height} />);
     };
     lines.push(
-            <Vline x={width/2} color={COLORS.RED} length={2000} />,
-            <Hline y={height/2} color={COLORS.RED} length={2000} />,
+            <Hline y={height/2} color={COLORS.RED} length={width} />,
+            <Vline x={width/2} color={COLORS.RED} length={height} />,
         );
     return (
         <Group>
@@ -44,7 +44,13 @@ export function GridNodes (props) {
     for (let x = 0; x <= data.x; x++) for (let y = 0; y <= data.y;y++) {
         let point = {x:x*data.gridUnit, y:y*data.gridUnit};
         let k = x*(data.y+1)+y;
-        nodes.push(<GridNode grid={[x,y]} key={k} clickHandler={clickHandler.bind(null, point)} location={[data.gridUnit * x, data.gridUnit * y]} size={data.gridUnit/2} />);
+        nodes.push(<GridNode
+            grid={[x,y]}
+            key={k}
+            clickHandler={clickHandler.bind(null, point)}
+            location={[(data.gridUnit * x)+2-data.gridUnit/2, (data.gridUnit * y)+2-data.gridUnit/2]}
+            size={data.gridUnit-4} />
+        );
     };
     return (
         <Group>
